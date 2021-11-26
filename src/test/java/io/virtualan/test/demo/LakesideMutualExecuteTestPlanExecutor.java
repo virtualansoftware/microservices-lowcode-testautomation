a3cf9a252bef7eb1fb230d4fc5501f44eb77d26a
elanj <elans3.java@gmail.com>
2021-11-26 09:00:34 -0600

    Keep track for feature file changes as well

--- src/test/java/io/virtualan/test/demo/LakesideMutualExecuteTestPlanExecutor.java
+++ src/test/java/io/virtualan/test/demo/LakesideMutualExecuteTestPlanExecutor.java
@@ -23,7 +23,7 @@ public class LakesideMutualExecuteTestPlanExecutor {
 
   // Using VIRTUALAN Collection
   // To run Performance testing of the system
-  @Test(threadPoolSize = 2, invocationCount = 2, timeOut = 10000)
+  @Test(threadPoolSize = 2, invocationCount = 1, timeOut = 10000)
   public void workflowExecution_1() {
     try {
       boolean isSuccess = VirtualanTestPlanExecutor
