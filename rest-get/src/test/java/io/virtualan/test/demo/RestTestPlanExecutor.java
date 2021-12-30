cfcfc0cab6f5a776e39197e7898f0ac4ffcf3cea
elanj <elans3.java@gmail.com>
2021-12-30 07:53:38 -0600

    Updated with Allure report configuration

--- rest-get/src/test/java/io/virtualan/test/demo/RestTestPlanExecutor.java
+++ rest-get/src/test/java/io/virtualan/test/demo/RestTestPlanExecutor.java
@@ -1,10 +1,7 @@
 package io.virtualan.test.demo;
 
-import io.restassured.RestAssured;
-import io.restassured.config.EncoderConfig;
 import io.virtualan.idaithalam.core.api.VirtualanTestPlanExecutor;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 
