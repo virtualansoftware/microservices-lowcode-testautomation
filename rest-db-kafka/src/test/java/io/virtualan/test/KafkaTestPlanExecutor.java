1e76c5c65e42b1e583f8459b3084e3b6f785815a
elanj <elans3.java@gmail.com>
2022-05-23 21:13:13 -0500

    Updated with 1.7.0 release

--- rest-db-kafka/src/test/java/io/virtualan/test/KafkaTestPlanExecutor.java
+++ rest-db-kafka/src/test/java/io/virtualan/test/KafkaTestPlanExecutor.java
@@ -1,6 +1,7 @@
 package io.virtualan.test;
 
 import io.virtualan.idaithalam.core.api.VirtualanTestPlanExecutor;
+import org.testng.Assert;
 
 public class KafkaTestPlanExecutor {
 
@@ -9,7 +10,7 @@ public class KafkaTestPlanExecutor {
     public void execute_orderEvent_workflow() {
         try {
             boolean isSuccess = VirtualanTestPlanExecutor.invoke("workflow/work-flow.yaml");
-            org.junit.Assert.assertTrue(isSuccess);
+            Assert.assertTrue(isSuccess);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -20,10 +21,19 @@ public class KafkaTestPlanExecutor {
     public void execute_orderEvent_protobuff_workflow() {
         try {
             boolean isSuccess = VirtualanTestPlanExecutor.invoke("workflow/work-flow-proto.yaml");
-            org.junit.Assert.assertTrue(isSuccess);
-
+            Assert.assertTrue(isSuccess);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+//    @org.testng.annotations.Test
+//    public void execute_userEvent_avro_workflow() {
+//        try {
+//            boolean isSuccess = VirtualanTestPlanExecutor.invoke("workflow/work-flow-avro.yaml");
+//            Assert.assertTrue(isSuccess);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
